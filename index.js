1a5139ff040b1c5e5c8a283a4c3e1f0b61705eae
Badol Roy <badolroy685@gmail.com>
2026-01-01 20:23:13 +0600

    added firebase

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const cors = require('cors');
 require('dotenv').config(); // Load environment variables from .env file
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port =process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-console.log( process.env.DB_USER);
-console.log( process.env.DB_PASSWORD);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
 
-const uri = `mongodb+srv://${ process.env.DB_USER}:${ process.env.DB_PASSWORD}@cluster0.ssfhyko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ssfhyko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 
@@ -31,12 +31,18 @@ async function run() {
     await client.connect();
 
     const recipeCollection = client.db("recipeDB").collection("recipes");
-    // const recipe = client.db("recipeDB").collection("recipe"); 
+    const userCollection = client.db("recipeDB").collection("users");
 
     app.get('/recipes', async (req, res) => {
-      // const cursor = recipeCollection.find();
-      // const result = await cursor.toArray();
+
       const result = await recipeCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/recipes/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await recipeCollection.findOne(query);
       res.send(result);
     });
 
@@ -51,6 +57,31 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await recipeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //user related apis
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.patch('/users', async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+      console.log(email, lastSignInTime);
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
